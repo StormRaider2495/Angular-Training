c4ec49f0da859efaca1a0facdb5d0d1095248659
LEARNINGMATEKOL\arunit.mazumdar <arunit.mazumdar@learningmate.com>
2018-05-05 19:30:28 +0530

    first-app updated

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GreeterComponent} from './greeter/greeter.component';
+import { CalculatorOneComponent } from './calculator-one/calculator-one.component';
+import { CalculatorTwoComponent } from './calculator-two/calculator-two.component';
+import { CalculatorResultComponent } from './calculator-result/calculator-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreeterComponent,
+    CalculatorOneComponent,
+    CalculatorTwoComponent,
+    CalculatorResultComponent
   ],
   imports: [
     BrowserModule
